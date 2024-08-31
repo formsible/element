43b2ed9e124d5c58416b814171c9d5d5f23aed2d
Sam <ns8448407@gmail.com>
2024-08-31 20:25:34 +0700

    implement sortable list element

--- src/components/list/index.vue
+++ src/components/list/index.vue
@@ -1,13 +1,56 @@
 <script setup lang="ts">
-import { ref, type Ref } from 'vue'
+import { PropType, ref, watch, type Ref } from 'vue'
 import { useSortable } from '@vueuse/integrations/useSortable'
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
 import DragIcon from '~icons/material-symbols/drag-handle'
 import TrashIcon from '~icons/material-symbols/delete'
+import { InputProperties } from '~/types'
 
-const list: Ref<{ label: string; id: string; editing: boolean }[]> = ref([])
+const props = defineProps({
+  input: {
+    type: Object as PropType<InputProperties>,
+    required: true,
+  },
+  theme: {
+    type: Object,
+    default: () => ({
+      container: 'flex flex-col gap-2',
+      label: 'w-full text-black dark:text-white', // Added text color for label
+      input: 'w-full h-32 bg-white dark:bg-gray-800 text-black dark:text-white', // Added background and text color for TextArea
+      description: 'text-sm text-slate-700 dark:text-slate-300',
+      error: 'text-red-600 dark:text-red-400', // Added dark mode color for error
+    }),
+  },
+  error: {
+    type: String,
+    default: '',
+  },
+})
+const model = defineModel<string[]>({ default: [] })
+
+const list: Ref<{ label: string; id: string }[]> = ref([])
 const listEl: Ref<HTMLElement | null> = ref(null)
+
+watch(
+  () => props.input.props,
+  () => {
+    list.value = (props.input?.props?.items || []).map((i) => {
+      return {
+        id: crypto.randomUUID(),
+        label: i,
+      }
+    })
+  },
+  { immediate: true },
+)
+watch(
+  list,
+  () => {
+    model.value = list.value.map((i) => i.label).filter((i) => !!i)
+  },
+  { deep: true, immediate: true },
+)
 
 useSortable(listEl, list, {
   handle: '.handle',
@@ -20,7 +63,6 @@ const removeItem = (index: number) => {
 
 const addItem = () => {
   list.value.push({
-    editing: true,
     id: crypto.randomUUID(),
     label: '',
   })
@@ -28,23 +70,15 @@ const addItem = () => {
 </script>
 
 <template>
-  <div class="text-start">
+  <div>
+    <p>{{ model }}</p>
     <div ref="listEl" class="flex flex-col gap-3 justify-center">
       <div
         v-for="(item, index) in list"
         :key="item.id"
         class="flex flex-row gap-2 items-center"
       >
-        <InputText
-          v-model="list[index].label"
-          placeholder="Label"
-          @blur="
-            () => {
-              if (item.label.length > 0) item.editing = false
-            }
-          "
-          class="w-full"
-        />
+        <InputText v-model="list[index].label" placeholder="Label" />
         <DragIcon class="handle cursor-move"></DragIcon>
         <button @click="removeItem(index)">
           <TrashIcon />
