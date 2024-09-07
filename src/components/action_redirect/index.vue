4f81669de0f673b99633d8ec3ddd6dd8ccec614b
tewnut <tewnut@gmail.com>
2024-09-07 14:27:11 +0700

    readonly

--- src/components/action_redirect/index.vue
+++ src/components/action_redirect/index.vue
@@ -15,11 +15,15 @@ const props = defineProps({
       cancelClass: 'bg-red-500 text-white font-bold py-2 px-4 rounded ml-2 hover:bg-red-600',
     }),
   },
+  readonly: {
+    type: Boolean, default: false
+  }
 })
 
 // Countdown timer and progress
 const countdown = ref(5)
 const progress = ref(100)
+// eslint-disable-next-line no-undef
 const interval = ref<NodeJS.Timeout | null>(null)
 const redirecting = ref(true)
 
@@ -30,7 +34,7 @@ onMounted(() => {
     progress.value = (countdown.value / 5) * 100
     if (countdown.value === 0) {
       clearInterval(interval.value!)
-      window.location.href = props.display.src as string
+      if (!props.readonly) window.location.href = props.display.src as string
     }
   }, 1000)
 })
@@ -53,12 +57,14 @@ onBeforeUnmount(() => {
 
 <template>
   <div class="p-4 max-w-md mx-auto">
+  <p>{{ props.readonly }}</p>
     <div v-if="redirecting">
-      <p class="text-lg mb-4">You are being redirected to <strong>{{ display.src }}</strong> in {{ countdown }} seconds.</p>
+      <p class="text-lg mb-4">You are being redirected to <strong>{{ display.src }}</strong> in {{ countdown }} seconds.
+      </p>
       <div class="relative w-full bg-gray-200 rounded-full h-2 mb-4">
         <div :class="theme.progressClass" :style="{ width: progress + '%' }"></div>
       </div>
-      <button :class="theme.buttonClass">
+      <button :class="theme.buttonClass" v-bind="$attrs">
         Redirecting...
       </button>
       <button :class="theme.cancelClass" @click="cancelRedirect">
